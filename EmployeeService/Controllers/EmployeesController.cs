4be3509378aaf1a315d2ea59e4fbeef6b7d676a2
Oloruntobi Junaid <easytobeh@gmail.com>
2021-06-04 12:28:50 +0100

    created data acccess model from DB

---
+++ EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace EmployeeService.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+
+        public HttpResponseMessage PostEmployee([FromBody] Employee employee)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    entities.Employees.Add(employee);
+                    entities.SaveChanges();
+
+                    //HttpResponseMessage res = new HttpResponseMessage(HttpStatusCode.Created);
+
+                    var response = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    response.Headers.Location = new Uri($"{Request.RequestUri}{employee.ID.ToString()}");
+                        
+                    return response;
+
+                }
+            }
+            catch (Exception Ex)
+            {
+
+                //var response = Request.CreateResponse(HttpStatusCode.InternalServerError);
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, Ex.Message);
+                
+            }
+
+        }
+
+    
+    }
+}
